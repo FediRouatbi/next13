20537e4f576dff54f7d5025b50bf50545b2c9c94
Fedi Rouatbi <fedirouatbi@gmail.com>
2023-10-07 16:16:39 +0100

    fix build error

--- sanity/schema.ts
+++
@@ -1,6 +0,0 @@
-import project from "@/sanity/projectSchema";
-import { type SchemaTypeDefinition } from "sanity";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [project],
-};
